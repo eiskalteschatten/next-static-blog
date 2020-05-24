244dad48a54707469d5f4999927464fe96409997
Alex <alex@alexseifert.com>
2020-05-24 13:46:14 +0200

    Display the meta data on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 import { getMetaDataForPosts, PostMetaData } from '../blog';
 
@@ -10,7 +12,23 @@ interface Props {
 const Home: React.FC<Props> = ({ postMetaData }) => {
   return (
     <>
-      {JSON.stringify(postMetaData)}
+      <div>
+        These are the latest 10 posts:
+      </div>
+
+      {postMetaData.map((metaData: PostMetaData, index: number) => (
+        <div key={index}>
+          <Link href={`/post/${metaData.slug}`} passHref>
+            <a>
+              <div>{metaData.title}</div>
+
+              {metaData.excerpt && (
+                <ReactMarkdown source={metaData.excerpt} />
+              )}
+            </a>
+          </Link>
+        </div>
+      ))}
     </>
   );
 };
