bb5e0559eb7a7eb2c531d187bd47a1c60fa1efe9
Alex <alex@alexseifert.com>
2020-05-24 11:33:37 +0200

    Start getting the post files

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -1,3 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
+const postsFolder = path.resolve(`${process.cwd()}/src/blog/posts`);
+
 export interface PostMetaData {
   id: string;
   slug: string;
@@ -12,6 +17,17 @@ export interface PostMetaData {
   excerpt?: string;
 }
 
+
+export const getAllPostFiles = (): Promise<string[]> =>
+  new Promise((resolve, reject): void => {
+    fs.readdir(postsFolder, async (error: Error, files: string[]): Promise<void> => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(files.reverse());
+    });
+  });
 
 export const getPostMetaData = async (postName: string): Promise<PostMetaData> => {
   return {
@@ -30,8 +46,19 @@ export const getPostMetaData = async (postName: string): Promise<PostMetaData> =
 };
 
 export const getMetaDataForPosts = async (count?: number): Promise<PostMetaData[]> => {
-  const postMetaData = await getPostMetaData('');
-  return [postMetaData];
+  let files = await getAllPostFiles();
+  const data: PostMetaData[] = [];
+
+  if (count) {
+    files = files.splice(0, count);
+  }
+
+  for (const file of files) {
+    const postMetaData = await getPostMetaData(file);
+    data.push(postMetaData);
+  }
+
+  return data;
 };
 
 export const getAllPosts = async (): Promise<any> => {
---
+++ src/blog/posts/2020-06-25_second-post.md
@@ -0,0 +1,17 @@
+---
+id: second-post
+slug: second-post
+title: Second Post
+author: Alex Seifert
+authorLink: https://www.alexseifert.com
+titledImage: /images/blog/posts/test-image.jpg
+description: This is for the meta-description tag in the header for SEO.
+categories: news, test category, another category
+tags: news tag, another tag
+publishedDate: 2020-06-25T07:38:13
+
+excerpt:
+This is a longer bit of text that can use **Markdown** and be used as an excerpt to be shown on other pages.
+---
+
+This is where the actual post is written.
