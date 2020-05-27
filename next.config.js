ad6001bf9b2df4c91e1c80aaa394fb315660f26e
Alex <alex@alexseifert.com>
2020-05-27 23:12:53 +0200

    Use Next's built-in support for .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-require('dotenv').config();
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
-
-module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-    return config;
-  }
-};
+module.exports = {};
