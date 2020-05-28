a8fb54ae811c7d5b057c7c0337eac3d8b9de1c65
Alex <alex@alexseifert.com>
2020-05-28 22:30:46 +0200

    Add archives button

--- src/components/posts/Post/index.tsx
+++ src/components/posts/Post/index.tsx
@@ -17,8 +17,8 @@ interface Props {
 
 const PostComponent: React.FC<Props> = ({ post }) => {
   const publishedDate = new Date(post.metaData.publishedDate);
-  // const publishedMonth = ('0' + (publishedDate.getMonth()+1)).slice(-2);
-  // const archivesLink = `/archives/${publishedDate.getFullYear()}/${publishedMonth}`;
+  const publishedMonth = ('0' + (publishedDate.getMonth()+1)).slice(-2);
+  const archivesLink = `/archives/${publishedDate.getFullYear()}/${publishedMonth}`;
 
   return (
     <>
@@ -37,7 +37,10 @@ const PostComponent: React.FC<Props> = ({ post }) => {
         </h1>
 
         <div className={styles.publishedDate}>
-          {publishedDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} at {publishedDate.toLocaleTimeString('en-US')}<br />
+          {publishedDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} at {publishedDate.toLocaleTimeString('en-US')}
+          <Link href={archivesLink} passHref>
+            <a className='btn btn-sm btn-light ml-3'>&#x2192;</a>
+          </Link>
         </div>
 
         <div className={styles.author}>
