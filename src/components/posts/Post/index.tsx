8fac22064f94e8ec0016b52322525a9032b859f7
Alex <alex@alexseifert.com>
2020-05-28 21:48:56 +0200

    Start styling the post page

---
+++ src/components/posts/Post/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+
+import Container from 'react-bootstrap/Container';
+
+import styles from './Post.module.scss';
+
+import categories from '../../../blog/categories';
+import { Post } from '../../../blog';
+import CodeBlock from '../../CodeBlock';
+import AuthorAvatar from '../../AuthorAvatar';
+
+interface Props {
+  post: Post;
+}
+
+const PostComponent: React.FC<Props> = ({ post }) => {
+  const publishedDate = new Date(post.metaData.publishedDate);
+  // const publishedMonth = ('0' + (publishedDate.getMonth()+1)).slice(-2);
+  // const archivesLink = `/archives/${publishedDate.getFullYear()}/${publishedMonth}`;
+
+  return (
+    <>
+      {post.metaData.titleImage && (
+        <div
+          className={styles.titleImage}
+          style={{
+            backgroundImage: `url('${post.metaData.titleImage}')`
+          }}
+        />
+      )}
+
+      <Container>
+        <h1>
+          {post.metaData.title}
+        </h1>
+
+        <div className={styles.publishedDate}>
+          {publishedDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} at {publishedDate.toLocaleTimeString('en-US')}<br />
+        </div>
+
+        <div className={styles.author}>
+          <AuthorAvatar authorKey={post.metaData.author} />
+        </div>
+
+        <div className='mb-5'>
+          <ReactMarkdown
+            source={post.body}
+            renderers={{ code: CodeBlock }}
+          />
+        </div>
+
+        <div>
+          Categories: {post.metaData.categories.map((category: string, index: number): any => (
+            <span key={index}>
+              <Link href={`/category/${categories[category].slug}`} passHref>
+                <a>{categories[category].name}</a>
+              </Link>,&nbsp;
+            </span>
+          ))}<br />
+          Tags: {JSON.stringify(post.metaData.tags)}
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default PostComponent;
--- src/pages/post/[...slug].tsx
+++ src/pages/post/[...slug].tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
-import Link from 'next/link';
 import Head from 'next/head';
-import ReactMarkdown from 'react-markdown';
 
 import useStadandardHeaderTags from '../../lib/useStandardHeaderTags';
 import { getPostFolders, Post, getPost, convertFolderNameToSlugParts, convertSlugToFolderName } from '../../blog';
-import categories from '../../blog/categories';
-import authors from '../../blog/authors';
-import CodeBlock from '../../components/CodeBlock';
+import PostComponent from '../../components/posts/Post';
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   if (!Array.isArray(params.slug)) {
@@ -43,53 +39,13 @@ interface Props {
 }
 
 const PostPage: React.FC<Props> = ({ post }) => {
-  const publishedDate = new Date(post.metaData.publishedDate);
-  const publishedMonth = ('0' + (publishedDate.getMonth()+1)).slice(-2);
-  const archivesLink = `/archives/${publishedDate.getFullYear()}/${publishedMonth}`;
-  const author = authors[post.metaData.author];
-
   return (
     <>
       <Head>
         {useStadandardHeaderTags(post.metaData.title, post.metaData.description)}
       </Head>
 
-      {post.metaData.titleImage && (
-        <div>
-          <img src={post.metaData.titleImage} alt={post.metaData.title} />
-        </div>
-      )}
-
-      <h1>
-        {post.metaData.title}
-      </h1>
-
-      <div>
-        Published: <Link href={archivesLink} passHref>
-          <a>
-            {publishedDate.toLocaleDateString()} at {publishedDate.toLocaleTimeString()}<br />
-          </a>
-        </Link>
-        Author: <Link href={`/author/${post.metaData.author}`} passHref>
-          <a>{author.name}</a>
-        </Link>
-        <br />
-        Categories: {post.metaData.categories.map((category: string, index: number): any => (
-          <span key={index}>
-            <Link href={`/category/${categories[category].slug}`} passHref>
-              <a>{categories[category].name}</a>
-            </Link>,&nbsp;
-          </span>
-        ))}<br />
-        Tags: {JSON.stringify(post.metaData.tags)}
-      </div>
-
-      <div>
-        <ReactMarkdown
-          source={post.body}
-          renderers={{ code: CodeBlock }}
-        />
-      </div>
+      <PostComponent post={post} />
     </>
   );
 };
