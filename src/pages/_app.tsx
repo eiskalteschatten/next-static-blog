2ef9aaa81c998015b718e3d20c96aba111b055b0
Alex <alex@alexseifert.com>
2020-05-30 23:17:38 +0200

    Remove Redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
 import 'nprogress/nprogress.css';
 import '~/scss/styles.scss';
 
-import store from '~/store';
 import Layout from '~/components/Layout';
 
 const App: any = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 };
 
