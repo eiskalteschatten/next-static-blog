ab122530235cd40105eb33dbba60c80a16f6f853
Alex Seifert <alex@alexseifert.com>
2022-07-14 13:10:06 +0200

    Fix bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import 'bootstrap/scss/bootstrap-grid.scss';
+import 'bootstrap/scss/bootstrap-reboot.scss';
+import 'bootstrap/scss/bootstrap-utilities.scss';
+
 import 'nprogress/nprogress.css';
 import '~/scss/styles.scss';
 
